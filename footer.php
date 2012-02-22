b26f49468092ae2df756249b262ddf292f80a408
Mike Kuzin <mike.kuzin@showclix.com>
2012-02-22 10:48:05 -0500

    G Analytics install

--- footer.php
+++ footer.php
@@ -9,21 +9,27 @@
 ?>
 
 	<footer>
-
 <?php
 	get_sidebar( 'footer' );
 ?>
-
     		<div class='wrap' style="width:960px; margin: 0 auto; padding: 15px 0;">
-                <span id="tos" style="font-size: 11px; color: #AAA;">
+                <span id="tos" style="font-size: 11px;color: #AAA;">
                     Use of ShowClix is subject to our <a href="http://www.showclix.com/tos">Terms of Service</a>.
                     Copyright &copy; <?= date('Y'); ?> ShowClix Inc.
                     All Rights Reserved.
                 </span>
             </div>
-
-
 	</footer>
 
-</body>
-</html>
+	<script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-1783789-3']);
+        _gaq.push(['_trackPageview']);
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
+
+</body></html>
